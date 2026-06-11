d78e987db379efea024ff7564eefec1b3379aa7a
JohansReisch <joh.sepulvedap@duocuc.cl>
2026-06-10 23:58:45 -0400

    Actualizacion de ports en api gateway para evitar colapsos en modo de prueba

--- STIMY-GAMEHUB-MICROSERVICE/STIMY-GAMEHUB-MICROSERVICE/biblioteca-service/src/main/java/com/duoc/stimy/gamehub/client/VideojuegoClient.java
+++ STIMY-GAMEHUB-MICROSERVICE/STIMY-GAMEHUB-MICROSERVICE/biblioteca-service/src/main/java/com/duoc/stimy/gamehub/client/VideojuegoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "videojuego-service", url = "http://localhost:8082/api/videojuegos")
+@FeignClient(name = "videojuego-service", url = "http://localhost:8092/api/videojuegos")
 public interface VideojuegoClient {
 
     @GetMapping("/{id}")
--- STIMY-GAMEHUB-MICROSERVICE/STIMY-GAMEHUB-MICROSERVICE/pago-service/src/main/java/com/duoc/stimy/gamehub/client/UsuarioClient.java
+++ STIMY-GAMEHUB-MICROSERVICE/STIMY-GAMEHUB-MICROSERVICE/pago-service/src/main/java/com/duoc/stimy/gamehub/client/UsuarioClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "usuario-service", url = "http://localhost:8081/api/usuarios")
+@FeignClient(name = "usuario-service", url = "http://localhost:8091/api/usuarios")
 public interface UsuarioClient {
 
     @GetMapping("/{id}")
